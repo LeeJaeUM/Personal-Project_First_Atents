53085962dfb83d73185f4a416b080222ffeb90ee
LeeJaeUng <lsam3333333@gmail.com>
2024-01-08 15:19:57 +0900

    박자 enum으로 나눔
    1박 2박 4박

--- Assets/02_Scripts/Enemy/EnemyHit.cs
+++ Assets/02_Scripts/Enemy/EnemyHit.cs
@@ -14,13 +14,7 @@ public class EnemyHit : MonoBehaviour
     private void Awake()
     {
         anim = GetComponent<Animator>();
-
-        //애니메이터에서 클립 길이 받아오기
-        //GetCurrentAnimatorClipInfo(0) : 애니메이터의 첫 번째 레이어의 클립 정보 받아오기
-        //GetCurrentAnimatorClipInfo(0)[0] : 애니메이터의 첫 번째 레이어에 있는 애니메이션 클립 중 첫 번째 클립의 정보
-        
         animLength = anim.GetCurrentAnimatorClipInfo(0)[0].clip.length;
-
     }
     private void Start()
     {
@@ -32,7 +26,7 @@ public class EnemyHit : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("PlayerAttack"))
         {
-            Debug.Log("Hited Enemy!!");
+            //Debug.Log("Hited Enemy!!");
             Die();
         }
     }
@@ -40,6 +34,7 @@ public class EnemyHit : MonoBehaviour
     private void Die()
     {
         anim.SetBool("Die", true);
+        StopAllCoroutines();
         StartCoroutine(Die_Move());
         Destroy(this.gameObject, animLength);
     }
--- Assets/02_Scripts/Enemy/EnemyMove.cs
+++ Assets/02_Scripts/Enemy/EnemyMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
@@ -10,11 +11,45 @@ public class EnemyMove : MonoBehaviour
 
     Animator anim;
     readonly int Move_String = Animator.StringToHash("Move");
+    readonly int Die_String = Animator.StringToHash("Die");
+
+    public float animLength;
+    public Vector3 deathPosition;
+
+    public float lifeTime = 10.0f;
+
+    IEnumerator MoveCoroutine;
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
-        StartCoroutine(OneMove()); 
+        animLength = anim.GetCurrentAnimatorClipInfo(0)[0].clip.length;
     }
+
+    private void Start()
+    {
+        MoveCoroutine = OneMove();
+        StartCoroutine(MoveCoroutine);
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Enemy touch Player_coL");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerAttack"))
+        {
+            //Debug.Log("Hited Enemy!!");
+            Die();
+        }
+    }
+
 
     IEnumerator OneMove()
     {
@@ -26,17 +61,42 @@ public class EnemyMove : MonoBehaviour
             anim.SetTrigger(Move_String);
             transform.position = targetPosition;
             isMoving = false;
-           
+
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Die()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        StopCoroutine(MoveCoroutine);
+        anim.SetBool(Die_String, true);
+        StartCoroutine(Die_Move());
+        Destroy(this.gameObject, animLength);
+    }
+
+    IEnumerator Die_Move()
+    {
+        float elapsedTime = 0f;
+        Vector3 startPosition = transform.position;
+
+        while (elapsedTime < animLength)
         {
-            Debug.Log("Enemy touch Player_coL");
+            // 현재 시간에 따른 보간 비율 계산
+            float t = elapsedTime / animLength;
+
+            // SmoothStep 함수를 사용하여 처음에는 빠르게, 나중에는 느리게 이동
+            float smoothT = Mathf.SmoothStep(0f, 1f, t);
+
+            // SmoothStep 함수로 계산된 보간 비율을 사용하여 부드럽게 이동
+            transform.position = Vector3.Lerp(startPosition, deathPosition, smoothT);
+
+            // 시간 업데이트
+            elapsedTime += Time.deltaTime;
+
+            // 한 프레임 대기
+            yield return null;
         }
-        
+        // 애니메이션이 끝난 후 필요한 처리 추가
+        // 예를 들어 오브젝트 비활성화, 파괴 등
     }
 
 }
